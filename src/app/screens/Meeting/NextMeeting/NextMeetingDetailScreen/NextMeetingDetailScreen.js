a353e03d07598cbe5f8f1795ee770faf865c5d58
Martin Červený <cervenymartin95@gmail.com>
2018-03-06 15:01:35 +0100

    upravy v nextmeetingdetailscreen

--- src/app/screens/Meeting/NextMeeting/NextMeetingDetailScreen/NextMeetingDetailScreen.js
+++ src/app/screens/Meeting/NextMeeting/NextMeetingDetailScreen/NextMeetingDetailScreen.js
@@ -1,18 +1,49 @@
 import React, {Component} from 'react';
-import { Container, Tab, Tabs, TabHeading, Icon, Text } from 'native-base';
+import { Container, Icon, Text, Card, CardItem, Left, Body, Content, Thumbnail, ListItem, Input, Button } from 'native-base';
 import NoteListItem from '../../../../components/NoteListItem/NoteListItem';
 import styles from './styles';
 
 class NextMeetingDetailScreen extends Component {
     render() {
         return (
-            <Container>
-                <Tabs>
-                    <Tab heading={ <TabHeading><Icon name="ios-paper" /></TabHeading>}>
-                        <NoteListItem />
-                    </Tab>
-                </Tabs>
-            </Container>
+            <Content>
+                <Card>
+                    <CardItem >
+                        <Left>
+                            <Thumbnail square size={50} source={require('../../../../../resources/images/person-flat.png')} />
+                            <Body>
+                                <Text>Jan Novotný</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
+                    <ListItem style={{ height: 60}}>
+                        <Body>
+                            <Input style={{ marginLeft: 10, marginTop: 5}} placeholder="Poznámka 1"/>
+                        </Body>
+                    </ListItem>
+                    <ListItem style={{ height: 60}}>
+                        <Body>
+                        <Input style={{ marginLeft: 10, marginTop: 5}} placeholder="Poznámka 2"/>
+                        </Body>
+                    </ListItem>
+                    <Button iconLeft transparent primary>
+                        <Icon name='ios-add-circle' style={{ color: '#2ecc71'}}/>
+                    </Button>
+                </Card>
+                <Card>
+                    <CardItem >
+                        <Left>
+                            <Thumbnail square size={50} source={require('../../../../../resources/images/person-flat.png')} />
+                            <Body>
+                            <Text>Peter Green</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
+                    <Button iconLeft transparent primary>
+                        <Icon name='ios-add-circle' style={{ color: '#2ecc71'}}/>
+                    </Button>
+                </Card>
+            </Content>
         );
     }
 }
